30efeb3e2cc24958a980b3b239c48938de7ffc51
Chia-Lun Yeh <sharonyeh940105@gmail.com>
2019-02-22 09:05:54 +0100

    add separate feature files for clarity

--- utils.py
+++ utils.py
@@ -13,53 +13,53 @@ from sklearn.model_selection import StratifiedShuffleSplit
 def cleanQuotations(text):
     # clean quotations
     text = re.sub(r'[`‘’‛⸂⸃⸌⸍⸜⸝]', "'", text)
-    text = re.sub(r'[„“]|(\'\')|(,,)', '"', text)
+    text = re.sub(r'[„“”]|(\'\')|(,,)', '"', text)
     return text
 
 def cleanText(text):  
     # remove URLs
-    text = re.sub(r'(www\S+)|(http\S+)|(href)', '', text)
+    text = re.sub(r'(www\S+)|(https?\S+)|(href)', ' ', text)
     # remove anything within {} or [] or ().
-    text = re.sub(r'\{[^}]*\}|\[[^]]*\]|\([^)]*\)', '', text)
+    text = re.sub(r'\{[^}]*\}|\[[^]]*\]|\([^)]*\)', ' ', text)
     # remove irrelevant news usage
-    text = re.sub(r'Getty [Ii]mages?|Getty|[Ff]ollow us on [Tt]witter|MORE:|ADVERTISEMENT|VIDEO', '', text)
+    text = re.sub(r'Getty [Ii]mages?|Getty|[Ff]ollow us on [Tt]witter|MORE:|ADVERTISEMENT|VIDEO', ' ', text)
     # remove @ or # tags or weird ......
-    text = re.sub(r'@\S+|#\S+|\.{2,}', '', text)
-    # remove multiple white spaces
-    text = re.sub(r' {2,}', ' ', text)
+    text = re.sub(r'@\S+|#\S+|\.{2,}', ' ', text)
     # remove newline in the beginning of the file
     text = text.lstrip().replace('\n','')
+    # remove multiple white spaces
+    re1 = re.compile(r'  +')
+    text = re1.sub(' ', text)
     return text
 
-def fixup(x):
+def fixup(text):
     '''
-    fix some HTML codes and white spaces
+    fix some HTML codes and white spaces (from Jeremy Howard)
     '''
-    re1 = re.compile(r'  +')
-    x = x.replace('#39;', "'").replace('amp;', '&').replace('#146;', "'").replace(
-        'nbsp;', ' ').replace('#36;', '$').replace('\\n', "\n").replace('quot;', "'").replace(
-        '<br />', "\n").replace('\\"', '"').replace('<unk>','u_n').replace(' @.@ ','.').replace(
-        ' @-@ ','-').replace('\\', ' \\ ')
-    return re1.sub(' ', html.unescape(x))
+    text = text.replace('#39;', "'").replace('amp;', '&').replace('#146;', "'").replace('nbsp;', ' ') \
+    .replace('#36;', '$').replace('\\n', "\n").replace('quot;', "'").replace('<br />', "\n").replace('\\"', '"')\
+    .replace('<unk>','u_n').replace(' @.@ ','.').replace(' @-@ ','-').replace('\\', ' \\ ')
+    return html.unescape(text)
 
-def read_glove(dim):
+def read_glove(path, dim):
     '''
     read the glove vectors
     dim: 100, 200, 300
     '''
-    df = pd.read_csv('../data/glove.6B.' + str(dim) + 'd.txt', sep=" ", quoting=3, header=None, index_col=0)
+    df = pd.read_csv(path + 'glove.6B.' + str(dim) + 'd.txt', sep=" ", quoting=3, header=None, index_col=0)
     glove = {key: val.values for key, val in df.T.items()}
     return glove
 
-def customTokenize(text):
+def customTokenize(text, rm_stopwords=False):
     '''
     lower, strip numbers and punctuation, remove stop words
     '''
     tokens = nltk.word_tokenize(text)
     tokens = [w.lower() for w in tokens]
     words = [word for word in tokens if word.isalpha()]
-    stop_words = set(stopwords.words('english'))
-    words = [w for w in words if not w in stop_words]
+    if rm_stopwords:
+        stop_words = set(stopwords.words('english'))
+        words = [w for w in words if not w in stop_words]
     return words
 
 def fixedTestSplit(labels):
